5b9b6a1f197d20d0efa92edfcc59801a5db15705
Arch <wbhaoui@gmail.com>
2023-08-03 19:14:22 +0100

    Extract todos_grid method

--- src/classes/DOMController.js
+++ src/classes/DOMController.js
@@ -9,15 +9,12 @@ class DOMController {
   init_display() {
     const project_list = document.createElement("ul");
     this.projects.forEach((project) => {
-      console.log(project);
       const project_tab = document.createElement("li");
       project_tab.textContent = project.name;
       project_list.appendChild(project_tab);
     });
     this.nav.prepend(project_list);
-    console.log(this.nav);
     const current_project = project_list.firstChild;
-    console.log(current_project);
     current_project.classList.add("active_project");
     this.switch_project(this.projects[0]);
   }
@@ -26,18 +23,20 @@ class DOMController {
     this.main.textContent = "";
     // Add header to main
     const header = document.createElement("h1");
-    console.log(project.name);
     header.textContent = project.name;
     this.main.appendChild(header);
     // Add grid of todos
+    const grid = this.todos_grid(project);
+    this.main.append(grid);
+  }
+  todos_grid(project) {
     const grid = document.createElement("div");
     grid.classList.add("todo_grid");
     const header_text = ["Title", "Description", "Due date", "Priority"];
     const headers = header_text.map((text) => {
       const header = document.createElement("div");
       header.textContent = text;
-      header.classList.add("grid_header")
-      console.log(header);
+      header.classList.add("grid_header");
       return header;
     });
     grid.append(...headers);
@@ -54,8 +53,8 @@ class DOMController {
         node.textContent = content;
         return node;
       });
-      grid.append(...content_cells)
+      grid.append(...content_cells);
     });
-    this.main.append(grid);
+    return grid;
   }
 }
